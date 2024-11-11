2de653598ccd951a4231b36556958ee7be796faa
jaeho <wogh001004@gmail.com>
2024-11-11 17:02:04 +0900

    1111

---
+++ vsjava/java/src/a1111/integer43p.java
@@ -0,0 +1,14 @@
+package a1111;
+
+public class integer43p {
+    public static void main(String[] args) {
+        int var1 = 0b1011;
+        System.out.println("var1: " + var1);
+        int var2 = 0206;
+        System.out.println("var2: " + var2);
+        int var3 = 365;
+        System.out.println("var3: " + var3);
+        int var4 = 0xb3;
+        System.out.println("var4: " + var4);
+    }
+}
